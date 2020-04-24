4e39fa8bf07bee62a52898b64f898d80172a24ea
William Buck <wsbuckiii@gmail.com>
2020-04-23 18:48:18 -0700

    change model

--- src/main.py
+++ src/main.py
@@ -18,40 +18,54 @@ IMAGE_DIR = './data/celeba'
 
 def make_generator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Dense(7 * 7 * 256, use_bias=False, input_shape=(100,)))
+    model.add(layers.Dense(4 * 4 * 1024, use_bias=False, input_shape=(100,)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Reshape((7, 7, 256)))
-    assert model.output_shape == (None, 7, 7, 256)  # None is the batch size
+    model.add(layers.Reshape((4, 4, 1024)))
+    assert model.output_shape == (None, 4, 4, 1024)  # None is the batch size
 
     model.add(layers.Conv2DTranspose(
-        128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
-    assert model.output_shape == (None, 7, 7, 128)
+        512, (8, 8), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 8, 8, 512)
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
     model.add(layers.Conv2DTranspose(
-        64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 14, 14, 64)
+        256, (16, 16), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 16, 16, 256)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    
+    model.add(layers.Conv2DTranspose(
+        128, (32, 32), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 32, 32, 128)
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2),
+    model.add(layers.Conv2DTranspose(3, (64, 64), strides=(2, 2),
                                      padding='same', use_bias=False, activation='tanh'))
-    assert model.output_shape == (None, 28, 28, 1)
+    assert model.output_shape == (None, 64, 64, 3)
 
     return model
 
 
 def make_discriminator_model():
     model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
-                            input_shape=[28, 28, 1]))
+    model.add(layers.Conv2D(128, (32, 32), strides=(2, 2), padding='same',
+                            input_shape=[64, 64, 3]))
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.Conv2D(256, (16, 16), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+    
+    model.add(layers.Conv2D(512, (8, 8), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+    
+    model.add(layers.Conv2D(1024, (4, 4), strides=(2, 2), padding='same'))
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
